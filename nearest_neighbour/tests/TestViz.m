19b9128a42c81ac7a38122f430b02ae7cd31426f
Yasin <yasin.zamani@gmail.com>
2017-04-15 20:49:29 +0430

    Add some methods

--- nearest_neighbour/tests/TestViz.m
+++ nearest_neighbour/tests/TestViz.m
@@ -17,15 +17,39 @@ classdef TestViz < matlab.unittest.TestCase
             addpath(parentFolder);
         end
     end
-    
+
     methods (TestMethodTeardown)
         function restorePath(testCase)
             % Restore original path
             path(testCase.originalPath);
         end
     end
-    
+
     methods (Test)
+        function test_figure(testCase)
+            % arrange
+            name = 'Test: Viz.figure';
+
+            % act
+            h = Viz.figure(name);
+            
+            % assert
+            testCase.assertEqual(h.Name, name);
+        end
+
+        function test_plotPoint(testCase)
+            % arrange
+            point = [0, 0];
+            color = 'red';
+            
+            % act
+            Viz.figure('Test: Viz.plotPoint');
+            Viz.plotPoint(point, color);
+            
+            % assert
+            testCase.assertTrue(true);
+        end
+
         function test_plotRandomWalk(testCase)
             % arrange
             filename = './data/1.mat';
@@ -34,7 +58,35 @@ classdef TestViz < matlab.unittest.TestCase
             maxDistance = sample.input.config.maxDistance;
 
             % act
+            Viz.figure('Test: plotRandomWalk');
             Viz.plotRandomWalk(points, maxDistance);
+            
+            % assert
+            testCase.assertTrue(true);
+        end
+        
+        function test_plotSomeRandomWalks(testCase)
+            % arrange
+            filenames = dir('./data/*.mat');
+            filenames = arrayfun(...
+                @(x) fullfile(x.folder, x.name), ...
+                filenames, ...
+                'UniformOutput', false ...
+            );
+        
+            % act
+            Viz.plotSomeRandomWalks(filenames);
+            
+            % assert
+            testCase.assertTrue(true);
+        end
+
+        function test_animateRandomWalk(testCase)
+            % arrange
+            filename = './data/1.mat';
+            
+            % act
+            Viz.animateRandomWalk(filename);
             
             % assert
             testCase.assertTrue(true);
