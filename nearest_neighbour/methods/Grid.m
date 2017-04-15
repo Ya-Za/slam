cb7f8f108f1e65f0c738a724b7eb21512cf80a53
Yasin <yasin.zamani@gmail.com>
2017-04-15 20:51:58 +0430

    Write first draft

--- nearest_neighbour/methods/Grid.m
+++ nearest_neighbour/methods/Grid.m
@@ -2,20 +2,49 @@ classdef Grid < BaseNN
     %Linear nearest neighbour method
     
     properties
+        grid
+        girdResolution
     end
     
     methods
-        function obj = Grid(intersectionObj, config)
+        function obj = Grid(intersectionObj, info)
             % Constructor
 
-            obj = obj@BaseNN(intersectionObj, config);
+            obj = obj@BaseNN(intersectionObj, info);
+            
+            % init
+            obj.gridResolution = sqrt(2);
+            maxDistance = obj.info.maxDistance;
+            gridLength = ceil(2 * maxDistance / obj.gridResolution);
+            obj.grid = cell(gridLength);
         end
         
         function output = query(obj, point)
+            % todo: `getNeighbours`
+            bottomLeft = floor(point / obj.gridResolution);
             output = [];
+            output = [output, obj.grid{bottomLeft(1), bottomLeft(1)}];
+            output = [output, obj.grid{bottomLeft(1) + 1, bottomLeft(1)}];
+            output = [output, obj.grid{bottomLeft(1), bottomLeft(1) + 1}];
+            output = [output, obj.grid{bottomLeft(1) + 1, bottomLeft(1) + 1}];
+            
             
             % add `point` to `points`
             obj.points{end + 1} = point;
+            % add index to `grid`
+            obj.addPointToGrid(point);
+        end
+        
+        function addPointToGrid(obj, point)
+            % get position of nearest node in `grid`
+            position = floor(...
+                point + (obj.gridResolution / 2) / ...
+                obj.gridResolution ...
+            );
+            
+            % todo: `sub2ind`
+            obj.gird{position(1), position(2)}(end + 1) = ...
+                numel(obj.points);
         end
     end
     
