0f2c7f05c43d1a95acec3d9a20a97b6b8fcab854
Yasin <yasin.zamani@gmail.com>
2017-04-18 18:31:07 +0430

    Use methods = struct(handler, params)

--- nearest_neighbour/MethodRunner.m
+++ nearest_neighbour/MethodRunner.m
@@ -8,101 +8,74 @@ classdef MethodRunner < handle
         %   Input nearest neighbours methods
         % - rootDir: char vector
         %   Path of root of output directory
-        % - inputDirs: cell array of char vector
-        %   Path of input directories with respect to `rootDir`
         % - intersectionObj: Intersection
         %   Has `haveIntersection` method
+        % - info: struct
+        %   Extra information such as `maxDistance`, `numberOfPoints`,
+        %   ...
 
         methods_
-        rootDir = './assets';
-        % todo: convert to `inputDir` just for one dir
-        inputDirs
-        intersectionObj = Intersection();
-        % todo: add `overwrite` property
+        rootDir
+        intersectionObj
         info
+        % todo: add `overwrite` property
     end
     
     methods
-        function init(obj)
-            % Initilize properties
-            
-            % inputDirs
-            % - if `inputDirs` is empty put name of sub-folders of `rootDir`
-            %   in it
-            if isempty(obj.inputDirs)
-                obj.inputDirs = dir(obj.rootDir);
-                % remove `.` and `..`
-                obj.inputDirs(1:2) = [];
-                % select just directories not files
-                obj.inputDirs = obj.inputDirs([obj.inputDirs.isdir]);
-                % select name of directories
-                obj.inputDirs = {obj.inputDirs.name};
-            end
-        end
-
         function run(obj)
             % Run
-            
-            % init
-            obj.init();
 
-            for indexOfInputDir = 1:numel(obj.inputDirs)
-                inputDir = fullfile(...
-                    obj.rootDir, ...
-                    obj.inputDirs{indexOfInputDir} ...
+            % list all of input files
+            filenames = dir(fullfile(...
+                obj.rootDir, ...
+                '*.mat' ...
+            ));
+
+            for indexOfFilenames = 1:numel(filenames)
+                filename = fullfile(...
+                    filenames(indexOfFilenames).folder, ...
+                    filenames(indexOfFilenames).name ...
                 );
 
-                % list all of input files
-                listing = dir(fullfile(...
-                    inputDir, ...
-                    '*.mat' ...
-                ));
+                % load input pionts
+                sample = load(filename);
+                points = sample.input.points;
+                numberOfPoints = size(points, 2);
 
-                for indexOfInputFile = 1:numel(listing)
-                    inputFile = fullfile(...
-                        listing(indexOfInputFile).folder, ...
-                        listing(indexOfInputFile).name ...
+                for indexOfMethod = 1:numel(obj.methods_)
+                    % init time
+                    % - begin time
+                    beginTime = cputime();
+                    methodHnadler = obj.methods_(indexOfMethod).handler;
+                    method = methodHnadler(...
+                        obj.intersectionObj, ...
+                        obj.info, ...
+                        obj.methods_(indexOfMethod).params ...
                     );
+                    % - end time
+                    initTime = cputime() - beginTime;
 
-                    % load input pionts
-                    sample = load(inputFile);
-                    points = sample.input.points;
-                    numberOfPoints = size(points, 2);
-
-                    for indexOfMethod = 1:numel(obj.methods_)
-                        % init time
+                    methodName = func2str(methodHnadler);
+                    % compute output
+                    outputs = cell(1, numberOfPoints);
+                    elapsedTimes = zeros(1, numberOfPoints);
+                    for indexOfPoint = 1:numberOfPoints
+                        point = points(:, indexOfPoint);
+                        % elapsed time
                         % - begin time
                         beginTime = cputime();
-                        methodHnadler = obj.methods_{indexOfMethod};
-                        method = methodHnadler(...
-                            obj.intersectionObj, ...
-                            obj.info ...
-                        );
+                        outputs{indexOfPoint} = method.query(point);
                         % - end time
-                        initTime = cputime() - beginTime;
-
-                        methodName = func2str(methodHnadler);
-                        % compute output
-                        outputs = cell(1, numberOfPoints);
-                        elapsedTimes = zeros(1, numberOfPoints);
-                        for indexOfPoint = 1:numberOfPoints
-                            point = points(:, indexOfPoint);
-                            % elapsed time
-                            % - begin time
-                            beginTime = cputime();
-                            outputs{indexOfPoint} = method.query(point);
-                            % - end time
-                            elapsedTimes(indexOfPoint) = ...
-                                cputime() - beginTime;
-                        end
-
-                        % save
-                        sample.output.(methodName).initTime = initTime;
-                        sample.output.(methodName).outputs = outputs;
-                        sample.output.(methodName).elapsedTimes = elapsedTimes;
-
-                        save(inputFile, '-struct', 'sample');
+                        elapsedTimes(indexOfPoint) = ...
+                            cputime() - beginTime;
                     end
+
+                    % save
+                    sample.output.(methodName).initTime = initTime;
+                    sample.output.(methodName).outputs = outputs;
+                    sample.output.(methodName).elapsedTimes = elapsedTimes;
+
+                    save(filename, '-struct', 'sample');
                 end
             end
         end
